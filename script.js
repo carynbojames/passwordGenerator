55bc5251394d4d7792f253083178fd2938787ac9
carynbojames <110750060+carynbojames@users.noreply.github.com>
2022-08-30 11:41:54 -0500

    Added conditional statements for user selected value and random type

--- script.js
+++ script.js
@@ -26,20 +26,20 @@ var writePassword = function() {
     // Restarts the code to select a different value 
     } else (selectLength());
     }
-    selectLength();
+  selectLength();
 
   // var passwordLower; 
   var selectLower = function() {
     // var passwordLower = window.prompt("Select if the password includes lowercase letters. Enter 'yes' or 'no'.");
-    passwordLower = window.prompt("Do you want lowercase?")
+    passwordLower = window.prompt("Select if the password includes lowercase letters. Enter 'yes' or 'no'.")
     if (passwordLower == "yes" || passwordLower == "no") {
       return console.log("Password Lowercase: " + passwordLower);
     } else (selectLower());
     }
-    selectLower();
+  selectLower();
 
   var selectUpper = function() {
-    var passwordUpper = window.prompt("Select if the password includes lowercase letters. Enter 'yes' or 'no'.")
+    passwordUpper = window.prompt("Select if the password includes upper letters. Enter 'yes' or 'no'.")
     if (passwordUpper == "yes" || passwordUpper === "no") {
       console.log("Password Upper: " + passwordUpper);
     } else (selectUpper());
@@ -47,7 +47,7 @@ var writePassword = function() {
   selectUpper();
 
   var selectNumber = function() {
-    var passwordNumber = window.prompt("Select if the password includes lowercase letters. Enter 'yes' or 'no'.")
+    passwordNumber = window.prompt("Select if the password includes numbers. Enter 'yes' or 'no'.")
     if (passwordNumber == "yes" || passwordNumber == "no") {
       console.log("Password Number: " + passwordNumber);
   } else (selectNumber()); 
@@ -55,47 +55,48 @@ var writePassword = function() {
   selectNumber();
 
   var selectSpecial = function() {
-    var passwordSpecial = window.prompt("Select if the password includes lowercase letters. Enter 'yes' or 'no'.")
+    passwordSpecial = window.prompt("Select if the password includes special characters. Enter 'yes' or 'no'.")
     if (passwordSpecial == "yes" || passwordSpecial == "no") {
       console.log("Password Special: " + passwordSpecial);
     } else (selectSpecial());
   }
   selectSpecial(); 
 
-  // Select random letter - upper
-  function selectUpper() {
-    var upperString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    return upperString[Math.floor(Math.random() * upperString.length)]
-  }
-  console.log("Upper String Random: " + selectUpper());
-
-  var upperString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  console.log("Upper String First: " + upperString[0]);
-  console.log("Upper String Last: " + upperString[25]);
-  console.log("Upper String Length: " + upperString.length);
-
   // Select random letter - lower
-  function selectLower() {
+  function randomLower() {
     var lowerString = "abcdefghijklmnopqrstuvwxyz"
     return lowerString[Math.floor(Math.random() * lowerString.length)]
   }
-  console.log("Lower String Random: " + selectLower());
+  console.log("Lower String Random: " + randomLower());
 
   var lowerString = "abcdefghijklmnopqrstuvwxyz"
   console.log("Lower String First: " + lowerString[0]);
   console.log("Lower String Last: " + lowerString[25]);
   console.log("Lower String Length: " + lowerString.length);
 
+  // Select random letter - upper
+  function randomUpper() {
+    var upperString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    return upperString[Math.floor(Math.random() * upperString.length)]
+  }
+  console.log("Upper String Random: " + randomUpper());
+
+  var upperString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  console.log("Upper String First: " + upperString[0]);
+  console.log("Upper String Last: " + upperString[25]);
+  console.log("Upper String Length: " + upperString.length);
+
+
   // Select random number
-  var selectNumber = Math.floor(Math.random() * 10);
-  console.log("Random Number: " + selectNumber);
+  var randomNumber = Math.floor(Math.random() * 10);
+  console.log("Random Number: " + randomNumber);
 
   // Select a special character
-  function selectSpecial() {
+  function randomSpecial() {
     var special = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
     return special[Math.floor(Math.random() * special.length)]
   }
-  console.log("Special Random: " + selectSpecial());
+  console.log("Special Random: " + randomSpecial());
 
   var special = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
   console.log("Special First: " + special[0]);
@@ -107,21 +108,33 @@ var writePassword = function() {
     var passwordArray = ["Upper", "Lower", "Number", "Special"];
     console.log(passwordArray);
     console.log("Password Array Length: " + passwordArray.length); 
-
     // Randomly select the position in the array 
-    var passwordXXXX = Math.floor(Math.random() * passwordArray.length)
-    return passwordCharacter = passwordArray[passwordXXXX]; 
-  }
-  passwordCharacter();
-  console.log("Character Type: " + passwordCharacter)
+    var passwordPosition = Math.floor(Math.random() * passwordArray.length)
+    return passwordCharacter = passwordArray[passwordPosition]; 
+    }
+    passwordCharacter();
+    console.log("Character Type: " + passwordCharacter)
 
 
   console.log("Password Length Check: " + passwordLength); 
+  console.log("Password Lower Check: " + passwordLower); 
+  console.log("Password Upper Check: " + passwordUpper); 
+  console.log("Password Number Check: " + passwordNumber); 
+  console.log("Password Special Check: " + passwordSpecial); 
   
   //  ACTION: Add conditional statement. If Upper == True && 
   if (passwordLower == "yes" && passwordCharacter == "Lower") {
-     console.log("Random Character: " + selectUpper); }
-
+    console.log("Random Character: " + randomLower()); 
+  } else if (passwordUpper == "yes" && passwordCharacter == "Upper") {
+    console.log("Random Character: " + randomUpper());
+  } else if (passwordNumber == "yes" && passwordCharacter == "Number") {
+    console.log("Random Character: " + randomNumber);
+  } else if (passwordSpecial == "yes" && passwordCharacter == "Special") {
+    console.log("Random Character: " + randomSpecial());
+  } // Reselect a random position in the array until one of the conditions above is true
+    else {
+  passwordCharacter(); 
+  }
 }
 
   // ACTION: Right now this just passes it forward. It doesn't return it to the main screen. 
@@ -135,16 +148,7 @@ generateBtn.addEventListener("click", writePassword);
 
 
  
-  // } else if (passwordUpper == "yes" && passwordCharacter == "Upper") {
-  //   console.log("Random Character: " + selectLower);
-  // } else if (passwordNumber == "yes" && passwordCharacter == "Number") {
-  //   console.log("Random Character: " + selectNumber);
-  // } else if (passwordSpecial == "yes" && passwordCharacter == "Special") {
-  //   console.log("Random Character: " + selectSpecial());
-  // } // Reselect a random position in the array until one of the conditions above is true
-  //   else {
-  //   passwordCharacter(); 
-  //   }
+
 
 
 // for (var i = 0; i < passwordLength.length; i++) {
